f4bb335aaee640aafce9c0910d0dd6e6880d6884
Hakan Altindag <hakangoudberg@hotmail.com>
2021-03-06 15:30:37 +0100

    Added logback classic example

---
+++ log-captor-examples/log-captor-with-logback-classic/src/test/java/nl/altindag/logback/FooServiceShould.java
@@ -0,0 +1,22 @@
+package nl.altindag.logback;
+
+import nl.altindag.log.LogCaptor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FooServiceShould {
+
+    @Test
+    void sayHello() {
+        LogCaptor logCaptor = LogCaptor.forClass(FooService.class);
+
+        FooService fooService = new FooService();
+        fooService.hello();
+
+        assertThat(logCaptor.getLogs())
+                .hasSize(1)
+                .contains("Hello there friend!");
+    }
+
+}
