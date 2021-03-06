acd06998dadf8bc33e9ed57fa451712de17b61dc
Hakan Altindag <hakangoudberg@hotmail.com>
2021-03-06 15:44:04 +0100

    Added slf4j with log4j example

---
+++ log-captor-examples/log-captor-with-slf4j-log4j/src/main/java/nl/altindag/log4j/FooService.java
@@ -0,0 +1,14 @@
+package nl.altindag.log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FooService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FooService.class);
+
+    public void hello() {
+        LOGGER.info("Hello there friend!");
+    }
+
+}
