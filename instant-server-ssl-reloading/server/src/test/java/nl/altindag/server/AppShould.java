f90a2fa2f423135f55bdf0e21f82f0be26ddf980
Hakan Altindag <hakangoudberg@hotmail.com>
2021-05-04 05:44:44 +0200

    Updated keystores

--- instant-server-ssl-reloading/server/src/test/java/nl/altindag/server/AppShould.java
+++ instant-server-ssl-reloading/server/src/test/java/nl/altindag/server/AppShould.java
@@ -30,7 +30,7 @@ class AppShould {
 
     @Test
     void updateServerCertificate() throws IOException, InterruptedException {
-        assertServerCertificateExpirationDate("2021-08-02");
+        assertServerCertificateExpirationDate("2029-05-02");
 
         SSLFactory sslFactory = createClientSllFactory();
 
